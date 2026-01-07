d3370aa84f1854517252bfcd97ab6b3449e3fe10
herehyun <82385436+herehyun@users.noreply.github.com>
2026-01-07 15:53:13 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: [
+      "devserver-master--dulcet-granita-3152be.netlify.app",
+      ".netlify.app" // Netlify 전체 허용 (권장)
+    ]
+  }
 });
